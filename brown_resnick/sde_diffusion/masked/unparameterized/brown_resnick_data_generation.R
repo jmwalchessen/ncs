bdbb43019947935a9d59f5d06082e2dba6d4b683
Julia <jwalches@andrew.cmu.edu>
2024-07-27 12:18:52 +1000

    br data generation make faster for niasra

--- brown_resnick/sde_diffusion/masked/unparameterized/brown_resnick_data_generation.R
+++ brown_resnick/sde_diffusion/masked/unparameterized/brown_resnick_data_generation.R
@@ -15,7 +15,7 @@ n.size <- 1024
 nn <- sqrt(n.size)
 x <- y <- seq(-10, 10, length = nn)
 coord <- expand.grid(x, y)
-number_of_replicates_per_call <- 50
+number_of_replicates_per_call <- 100
 calls <- as.integer(number_of_replicates/number_of_replicates_per_call)
 repnumberslist <- rep(number_of_replicates_per_call, calls)
 
@@ -36,7 +36,7 @@ collect_data <- function(parallel_output, nn, number_of_replicates_per_call)
     return(y)
 }
 
-cores <- ((detectCores(logical = TRUE))-100)
+cores <- ((detectCores(logical = TRUE)))
 y <- mclapply(repnumberslist, function(repsnumber)
 simulate_data_per_core(repsnumber, nn, coord, range, smooth), mc.cores = cores)
 y <- collect_data(y, nn, number_of_replicates_per_call)
