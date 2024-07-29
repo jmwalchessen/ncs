2c231469e71edee25d5727a197441c25ab9a86b9
JuliaPC <localpc@juliaPC.lan.uow.edu.au>
2024-07-29 10:26:14 +1000

    model 5 gp classifier data generation

--- validation/unparameterized/unconditional/generate_data/diffusion_data_generation.py
+++ validation/unparameterized/unconditional/generate_data/diffusion_data_generation.py
@@ -5,11 +5,7 @@ import os
 from append_directories import *
 
 home_folder = append_directory(5)
-twisted_diffusion_folder = home_folder + "/twisted_diffusion/image_exp/image_diffusion"
-sde_folder = home_folder + "/sde_diffusion"
-sys.path.append(twisted_diffusion_folder)
-import my_smc_diffusion
-import my_feynman_kac_image_ddpm
+sde_folder = home_folder + "/sde_diffusion/masked/unparameterized"
 #sde configs folder
 sde_configs_vp_folder = sde_folder + "/configs/vp"
 sys.path.append(sde_configs_vp_folder)
@@ -17,9 +13,10 @@ import ncsnpp_config
 sys.path.append(sde_folder)
 from models import ncsnpp
 import time
+import sde_lib
 
 n = 32
-T = 250
+T = 1000
 device = "cuda:0"
 #get trained score model
 config = ncsnpp_config.get_config()
@@ -30,37 +27,71 @@ score_model = (ncsnpp.NCSNpp(config)).to("cuda:0")
 score_model.load_state_dict(th.load((sde_folder + "/trained_score_models/model5_beta_min_max_01_20_random050_masks.pth")))
 score_model.eval()
 
-#get smc diffusion sampler
-betas = my_smc_diffusion.get_named_beta_schedule("linear", T)
-twisted_diffusion_model = my_feynman_kac_image_ddpm.TwistedDDPM(betas = betas, particle_base_shape = (1,32,32),
-                        rescale_timesteps = False, conf=None,
-                        probability_flow = False, device = device,
-                        use_timesteps = [i for i in range(0,250)],
-                        original_num_steps = 250)
+sdevp = sde_lib.VPSDE(beta_min=0.1, beta_max=20, N=1000)
+#y is observed part of field
+def p_mean_and_variance_from_score_via_mask(vpsde, score_model, device, masked_xt, mask, y, t):
 
-def sample_unconditionally(diffusion_model, trained_score_model, replicates_per_call,
-                           calls, n):
+    num_samples = masked_xt.shape[0]
+    timestep = ((th.tensor([t])).repeat(num_samples)).to(device)
+    with th.no_grad():
+        score = score_model(masked_xt, timestep)
+    unmasked_p_mean = (1/th.sqrt(th.tensor(vpsde.alphas[t])))*(masked_xt + th.square(th.tensor(vpsde.sigmas[t]))*score)
+    masked_p_mean = th.mul((1-mask), unmasked_p_mean) + th.mul(mask, y)
+    unmasked_p_variance = (th.square(th.tensor(vpsde.sigmas[t])))*th.ones_like(masked_xt)
+    masked_p_variance = th.mul((1-mask), unmasked_p_variance)
+    return masked_p_mean, masked_p_variance
+
+def sample_with_p_mean_variance_via_mask(vpsde, score_model, device, masked_xt, mask, y, t, num_samples):
+
+    p_mean, p_variance = p_mean_and_variance_from_score_via_mask(vpsde, score_model, device, masked_xt, mask, y, t)
+    std = th.exp(0.5 * th.log(p_variance))
+    noise = th.randn_like(masked_xt)
+    #just to make sure that the masked values aren't perturbed by the noise, the variance should already be masked though
+    masked_noise = th.mul((1-mask), noise)
+    sample = p_mean + std*masked_noise
+    return sample
+
+
+def posterior_sample_with_p_mean_variance_via_mask(vpsde, score_model, device, mask,
+                                                   y, n, num_samples):
+
+    unmasked_xT = th.randn((num_samples, 1, n, n)).to(device)
+    masked_xT = th.mul((1-mask), unmasked_xT) + th.mul(mask, y)
+    masked_xt = masked_xT
+    for t in range((vpsde.N-1), 0, -1):
+        masked_xt = sample_with_p_mean_variance_via_mask(vpsde, score_model, device, masked_xt,
+                                                         mask, y, t, num_samples)
+
+    return masked_xt
+
+def sample_unconditionally_multiple_calls(vpsde, score_model, device, mask, y, n,
+                                          num_samples_per_call, calls):
     
-    unconditional_samples = th.zeros((0,n,n))
-    for i in range(0, calls):
-        current_samples = diffusion_model.posterior_sample_with_p_mean_variance(trained_score_model,
-                                                                                replicates_per_call)
-        unconditional_samples = th.cat([unconditional_samples,
-                                        (current_samples.reshape((replicates_per_call,n,n))).detach().cpu()],
-                                        dim = 0)
-   
-    return unconditional_samples
+    diffusion_samples = th.zeros((0, 1, n, n))
+    for call in range(0, calls):
+        current_diffusion_samples = posterior_sample_with_p_mean_variance_via_mask(vpsde, score_model,
+                                                                                   device, mask, y, n,
+                                                                                   num_samples_per_call)
+        diffusion_samples = th.cat([current_diffusion_samples.detach().cpu(),
+                                    diffusion_samples],
+                                    dim = 0)
+    return diffusion_samples
+    
 
 
-replicates_per_call = 500
-calls = 200
-start = time.time()
-unconditional_samples = sample_unconditionally(twisted_diffusion_model, score_model, replicates_per_call,
-                           calls, n)
-end = time.time()
-print(print(end - start))
 
-np.save("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_100000.npy", unconditional_samples.numpy())
+for i in range(0, 100):
+    replicates_per_call = 500
+    calls = 2
+    p = 0
+    mask = (th.bernoulli(p*th.ones(1,1,n,n))).to(device)
+    start = time.time()
+    unconditional_samples = sample_unconditionally_multiple_calls(sdevp, score_model, device, mask, y, n,
+                                          replicates_per_call, calls)
+    end = time.time()
+    print(print(end - start))
+
+    np.save(("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_1000_" + str(i) + ".npy"), unconditional_samples.numpy())
 
 
 
