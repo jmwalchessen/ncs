cee83008de7094fc89fb3643dd093eca41b3d84a
Julia <jwalches@andrew.cmu.edu>
2024-07-30 10:54:17 +1000

    train data concatenate nn gp unconditional classifier

--- validation/unparameterized/unconditional/generate_data/concatenation.py
+++ validation/unparameterized/unconditional/generate_data/concatenation.py
@@ -2,8 +2,8 @@ import numpy as np
 
 n = 32
 conc = np.zeros((0,1,n,n))
-for i in range(0, 4):
+for i in range(0, 100):
     a = np.load("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_1000_" + str(i) + ".npy")
     conc = np.concatenate([conc,a], axis = 0)
 
-np.save("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_25000.npy", conc)
+np.save("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_100000.npy", conc)
