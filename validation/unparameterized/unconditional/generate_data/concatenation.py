dfbd0ec0e399660a705db55a059a3cc42d031a66
Julia <jwalches@andrew.cmu.edu>
2024-07-30 10:55:46 +1000

    create eval data gp unconditional classifier model 5

--- validation/unparameterized/unconditional/generate_data/concatenation.py
+++ validation/unparameterized/unconditional/generate_data/concatenation.py
@@ -2,8 +2,8 @@ import numpy as np
 
 n = 32
 conc = np.zeros((0,1,n,n))
-for i in range(0, 100):
-    a = np.load("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_1000_" + str(i) + ".npy")
+for i in range(0, 3):
+    a = np.load("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_1000_eval_" + str(i) + ".npy")
     conc = np.concatenate([conc,a], axis = 0)
 
-np.save("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_100000.npy", conc)
+np.save("data/diffusion/model5_unconditional_lengthscale_1.6_variance_0.4_eval_3000.npy", conc)
