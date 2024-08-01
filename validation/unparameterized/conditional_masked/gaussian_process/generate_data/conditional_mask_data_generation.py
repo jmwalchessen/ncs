c7e94d732abd77cb39964f8bf4e17d647fdccab7
JuliaPC <localpc@juliaPC.lan.uow.edu.au>
2024-08-01 14:34:19 +1000

    validation data for gp model 10

--- validation/unparameterized/conditional_masked/gaussian_process/generate_data/conditional_mask_data_generation.py
+++ validation/unparameterized/conditional_masked/gaussian_process/generate_data/conditional_mask_data_generation.py
@@ -24,14 +24,14 @@ device = "cuda:0"
 
 #get trained score model
 config = ncsnpp_config.get_config()
-config.model.num_scales = 1000
-config.model.beta_max = 20
+config.model.num_scales = 250
+config.model.beta_max = 25
 
 score_model = th.nn.DataParallel((ncsnpp.NCSNpp(config)).to("cuda:0"))
 score_model.load_state_dict(th.load((sde_folder + "/trained_score_models/vpsde/model4_beta_min_max_01_20_random50_masks.pth")))
 score_model.eval()
 
-sdevp = sde_lib.VPSDE(beta_min=0.1, beta_max=20, N=1000)
+sdevp = sde_lib.VPSDE(beta_min=0.1, beta_max=25, N=250)
 
 #mask is a True/False (1,32,32) vector with .5 randomly missing pixels
 #function gen_mask is in image_utils.py, 50 at end of random50 denotes
@@ -119,7 +119,7 @@ ref_img = generate_true_conditional_samples.generate_gaussian_process(minX, maxX
                                                                       number_of_replicates,
                                                                       seed_value)
 ref_img = th.from_numpy(ref_img[1].reshape((1,n,n))).to(device)
-p = .0025
+p = .5
 mask = (th.bernoulli(p*th.ones(1,1,n,n))).to(device)
 
 for i in range(0, 4):
@@ -130,16 +130,16 @@ for i in range(0, 4):
                                           replicates_per_call, calls)
 
     partially_observed = (mask*ref_img).detach().cpu().numpy().reshape((n,n))
-    np.save("data/gpmodel9/ref_image1/ref_image1.npy", ref_img.detach().cpu().numpy().reshape((n,n)))
-    np.save("data/gpmodel9/ref_image1/diffusion/model9_beta_min_max_01_25_random0025_250_" + str(i) + ".npy", conditional_samples)
-    np.save("data/gpmodel9/ref_image1/partially_observed_field.npy", partially_observed.reshape((n,n)))
-    np.save("data/gpmodel9/ref_image1/mask.npy", mask.int().detach().cpu().numpy().reshape((n,n)))
-    np.save("data/gpmodel9/ref_image1/seed_value.npy", np.array([int(seed_value)]))
+    np.save("data/gpmodel10/ref_image1/ref_image1.npy", ref_img.detach().cpu().numpy().reshape((n,n)))
+    np.save("data/gpmodel10/ref_image1/diffusion/model10_beta_min_max_01_25_random50_250_" + str(i) + ".npy", conditional_samples)
+    np.save("data/gpmodel10/ref_image1/partially_observed_field.npy", partially_observed.reshape((n,n)))
+    np.save("data/gpmodel10/ref_image1/mask.npy", mask.int().detach().cpu().numpy().reshape((n,n)))
+    np.save("data/gpmodel10/ref_image1/seed_value.npy", np.array([int(seed_value)]))
 
-    plot_spatial_field(ref_img.detach().cpu().numpy().reshape((n,n)), -3, 3, "data/gpmodel9/ref_image1/ref_image.png")
-    plot_spatial_field((conditional_samples[0,:,:,:]).numpy().reshape((n,n)), -3, 3, "data/gpmodel9/ref_image1/diffusion/visualizations/conditional_sample_0.png")
+    plot_spatial_field(ref_img.detach().cpu().numpy().reshape((n,n)), -3, 3, "data/gpmodel10/ref_image1/ref_image.png")
+    plot_spatial_field((conditional_samples[0,:,:,:]).numpy().reshape((n,n)), -3, 3, "data/gpmodel10/ref_image1/diffusion/visualizations/conditional_sample_0.png")
     plot_masked_spatial_field(spatial_field = ref_img.detach().cpu().numpy().reshape((n,n)),
-                   vmin = -2, vmax = 2, mask = mask.int().float().detach().cpu().numpy().reshape((n,n)), figname = "data/gpmodel9/ref_image1/partially_observed_field.png")
+                   vmin = -2, vmax = 2, mask = mask.int().float().detach().cpu().numpy().reshape((n,n)), figname = "data/gpmodel10/ref_image1/partially_observed_field.png")
 
 
 
