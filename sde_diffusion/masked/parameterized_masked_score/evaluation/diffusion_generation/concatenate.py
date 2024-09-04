be5bec979c182cea1df04eb7eb4de7669d0771ae
JuliaPC <localpc@juliaPC.lan.uow.edu.au>
2024-09-04 10:46:33 +1000

    evaluation files for parameterized masked score gp

---
+++ sde_diffusion/masked/parameterized_masked_score/evaluation/diffusion_generation/concatenate.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+n = 32
+samples = np.zeros((0,1,n,n))
+for i in range(0,4):
+    current_samples = np.load("data/model6/ref_image3/diffusion/model6_beta_min_max_01_20_random10_250_" + str(i) + ".npy")
+    samples = np.concatenate([samples, current_samples], axis = 0)
+
+np.save("data/model6/ref_image3/diffusion/model6_random10_beta_min_max_01_20_1000.npy", samples)
