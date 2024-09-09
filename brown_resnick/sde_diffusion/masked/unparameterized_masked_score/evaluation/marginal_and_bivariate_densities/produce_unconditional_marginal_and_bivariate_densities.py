9913b7498cda887820a8a21e8e41f032ef3611ea
JuliaPC <localpc@juliaPC.lan.uow.edu.au>
2024-09-09 17:15:47 +1000

    update local

--- brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/marginal_and_bivariate_densities/produce_unconditional_marginal_and_bivariate_densities.py
+++ brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/marginal_and_bivariate_densities/produce_unconditional_marginal_and_bivariate_densities.py
@@ -130,26 +130,26 @@ def produce_true_and_generated_bivariate_density(n, range_value, smooth_value,
 
 
 n = 32
-range_value = 1.6
+range_value = 3
 smooth_value = 1.6
 number_of_replicates = 4000
 matrix_index = (15,15)
-seed_value = 293571
-unconditional_true_samples = np.load("brown_resnick_samples_range_1.6_smooth_1.6_4000.npy")
-unconditional_generated_samples = np.load(diffusion_generation_folder + "/data/model2/ref_image1/diffusion/model2_random0_beta_min_max_01_20_1000.npy")
-"""
+seed_value = 201852
+unconditional_true_samples = np.load("schlather_samples_range_3_smooth_1.6_4000.npy")
+unconditional_generated_samples = np.load(diffusion_generation_folder + "/data/schlather/model2/ref_image1/diffusion/model2_random0_range_3_smooth_1.6_beta_min_max_01_20_1000.npy")
+
 for i in range(0, n, 4):
     for j in range(0, n, 4):
 
         matrix_index = (i,j)
         seed_value = np.random.randint(0, 100000, 1)[0]
-        figname = (diffusion_generation_folder + "/data/model2/ref_image1/marginal_density/model2_marginal_density_" + str(i) + "_" + str(j) + ".png")
+        figname = (diffusion_generation_folder + "/data/schlather/model2/ref_image1/marginal_density/model2_marginal_density_" + str(i) + "_" + str(j) + ".png")
         produce_true_and_generated_marginal_density(n, range_value, smooth_value, number_of_replicates,
                                                     matrix_index, seed_value, unconditional_generated_samples,
                                                     unconditional_true_samples, figname)
 
 
-"""
+
 
 indices1 = [(15+i,18) for i in range(-4,4)]
 indices2 = [(15,15)]
@@ -159,7 +159,7 @@ for i in indices1:
     for j in indices2:
         iindex = matrix_index_to_index(i, n)
         jindex = matrix_index_to_index(j, n)
-        folder_name = (diffusion_generation_folder + "/data/model2/ref_image1/bivariate_density/")
+        folder_name = (diffusion_generation_folder + "/data/schlather/model2/ref_image1/bivariate_density/")
         figname = (folder_name + "/bivariate_density_model2_" + str(iindex) + "_" + str(jindex) + ".png")
         seed_value = np.random.randint(0, 100000, 1)[0]
         produce_true_and_generated_bivariate_density(n, range_value, smooth_value,
