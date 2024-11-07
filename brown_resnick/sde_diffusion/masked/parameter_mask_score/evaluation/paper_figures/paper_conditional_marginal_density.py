d1e6083feaae80f1a3b8d7538d2340b6871f23ae
Julia <jwalches@andrew.cmu.edu>
2024-11-07 22:20:04 +1100

    bivariate fig

---
+++ brown_resnick/sde_diffusion/masked/parameter_mask_score/evaluation/paper_figures/paper_conditional_marginal_density.py
@@ -0,0 +1,68 @@
+import numpy as np
+from generate_true_conditional_samples import *
+from append_directories import *
+from mpl_toolkits.axes_grid1 import ImageGrid
+import matplotlib.pyplot as plt
+from paper_figure_helper_functions import *
+import seaborn as sns
+from matplotlib import gridspec
+
+def produce_marginal_density(model_name, image_name, file_name, missing_index, n, nrep, variance, lengthscale):
+
+    diffusion_images = load_diffusion_images(model_name, image_name, file_name)
+    mask = load_mask(model_name, image_name)
+    observations = load_observations(model_name, image_name, mask, n)
+    diffusion_marginal_density = (diffusion_images.reshape((nrep,n**2)))[:,missing_index]
+    minX = minY = -10
+    maxX = maxY = 10
+    true_images = sample_conditional_distribution(mask, minX, maxX, minY, maxY, n, variance, lengthscale, observations, nrep)
+    true_images = concatenate_observed_and_kriging_samples(observations, true_images, mask, n)
+    true_marginal_density = (true_images.reshape((nrep,n**2)))[:,missing_index]
+    return true_marginal_density, diffusion_marginal_density
+
+def visualize_marginal_density(model_name, missing_indices, n, nrep, variance, figname):
+
+    lengthscales = [1.0,2.0,3.0,4.0,5.0]
+    masks = np.zeros((5,n,n))
+    reference_images = np.zeros((5,n,n))
+    marginal_densities = np.zeros((5,nrep))
+    diffusion_marginal_densities = np.zeros((5,nrep))
+    for i in range(0, 5):
+        file_name = (model_name + "_variance_" + str(variance) + "_lengthscale_" + str(lengthscales[i]) + "_beta_min_max_01_20_random50_1000")
+        image_name = "ref_image" + str(i)
+        masks[i,:,:] = load_mask(model_name, image_name)
+        reference_images[i,:,:] = load_reference_image(model_name, image_name)
+        true_marginal_density, diffusion_marginal_density = produce_marginal_density(model_name, image_name, file_name,
+                                                                                     missing_indices[i], n, nrep, variance,
+                                                                                     lengthscales[i])
+        marginal_densities[i,:] = true_marginal_density
+        diffusion_marginal_densities[i,:] = diffusion_marginal_density
+
+
+    fig, axs = plt.subplots(ncols = 5, nrows = 2, figsize = (20,8))
+    for i in range(0,10):
+        if(i < 5):
+            matrix_index = index_to_matrix_index(missing_indices[i], n)
+            im = axs[int(i/5),int(i%5)].imshow(reference_images[i,:,:], cmap = 'viridis', vmin = -4, vmax = 4, alpha = masks[i,:,:].astype(float))
+            axs[int(i/5),int(i%5)].plot(matrix_index[1], matrix_index[0], "ro", markersize = 10, linewidth = 20)
+            axs[int(i/5),int(i%5)].set_xticks(ticks = [0, 8, 16, 24, 31], labels = np.array([-10,-5,0,5,10]))
+            axs[int(i/5),int(i%5)].set_yticks(ticks = [0, 8, 16, 24, 31], labels = np.array([-10,-5,0,5,10]))
+        else:
+            sns.kdeplot(marginal_densities[(i % 5),:], ax = axs[int(i/5),int(i%5)], color = "blue")
+            sns.kdeplot(diffusion_marginal_densities[(i % 5),:], ax = axs[int(i/5),int(i%5)], color = 'orange', linestyle = '--')
+            axs[int(i/5),int(i%5)].axvline(reference_images[(i%5),matrix_index[0],matrix_index[1]], color='red', linestyle = 'dashed')
+            axs[int(i/5),int(i%5)].set_xlim([-4.5,4.5])
+            axs[int(i/5),int(i%5)].set_ylim([0,1])
+            axs[int(i/5),int(i%5)].legend(labels = ['true', 'NCS'])
+
+    fig.colorbar(im, ax=axs, shrink = 1)
+    plt.savefig(figname)
+    plt.clf()
+
+model_name = "model6"
+missing_indices = [838, 700, 200, 301, 118]
+n = 32
+nrep = 1000
+variance = 1.5
+figname = "figures/gp_parameter_marginal_density.png"
+visualize_marginal_density(model_name, missing_indices, n, nrep, variance, figname)
---
+++ brown_resnick/sde_diffusion/masked/parameter_mask_score/evaluation/paper_figures/paper_visualizations.py
@@ -0,0 +1,71 @@
+import numpy as np
+import torch as th
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+import os
+import sys
+from append_directories import *
+from paper_figure_helper_functions import *
+
+
+def concatenate_observed_and_kriging_sample(observed, conditional_unobserved_sample, mask, n):
+
+    conditional_sample = np.zeros((n**2))
+    observed_indices = np.argwhere(mask.flatten() == 1)
+    missing_indices = np.argwhere(mask.flatten() == 0)
+    m = observed.shape[0]
+    conditional_sample[missing_indices] = conditional_unobserved_sample.reshape(((n**2-m),1))
+    conditional_sample[observed_indices] = observed.reshape((m,1))
+    conditional_sample = conditional_sample.reshape((n,n))
+    return conditional_sample
+
+def visualize_observed_and_diffusion(smooth, figname, n, model_name):
+
+    diffusion_visualizations = np.zeros((5,n,n))
+    true_visualizations = np.zeros((5,n,n))
+    reference_visualizations = np.zeros((5,n,n))
+    masks = np.zeros((5,n,n))
+    minX = minY = -10
+    maxX = maxY = 10
+    range_values = [1.0,2.0,3.0,4.0,5.0]
+    for i in range(0, 5):
+        image_name = "ref_image" + str(i)
+        ref_image = load_reference_image(model_name, image_name)
+        mask = load_mask(model_name, image_name)
+        partial_field = np.multiply(mask, ref_image)
+        y = load_observations(model_name, image_name, mask, n)
+        file_name = (model_name + "_range_" + str(range_values[i]) + "_smooth_" + str(smooth) + "_random0.5_4000")
+        diffusion_images = load_diffusion_images(model_name, image_name, file_name)
+        diffusion_visualizations[i,:,:] = (diffusion_images[20,:,:,:]).reshape((n,n))
+        reference_visualizations[i,:,:] = ref_image
+        masks[i,:,:] = mask
+
+    fig = plt.figure(figsize=(10,4))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                 nrows_ncols=(2, 5),  # creates 2x2 grid of Axes
+                 axes_pad=0.1,  # pad between Axes in inch.
+                 cbar_mode="single"
+                 )
+    
+    for i, ax in enumerate(grid):
+
+        if(i < 5):
+            im = ax.imshow(reference_visualizations[(i % 5),:,:], cmap='viridis', vmin = -2, vmax = 6,
+                           alpha = (masks[i,:,:].astype(float)))
+            ax.set_xticks(ticks = [0, 8, 16, 24, 31], labels = np.array([-10,-5,0,5,10]))
+            ax.set_yticks(ticks = [0, 8, 16, 24, 31], labels = np.array([-10,-5,0,5,10]))
+        else:
+            im = ax.imshow(diffusion_visualizations[(i % 5),:,:], cmap='viridis', vmin = -2, vmax = 6)
+            ax.set_xticks(ticks = [0, 8, 16, 24, 31], labels = np.array([-10,-5,0,5,10]))
+            ax.set_yticks(ticks = [0, 8, 16, 24, 31], labels = np.array([-10,-5,0,5,10]))
+
+
+    ax.cax.colorbar(im)
+    plt.savefig(figname)
+
+smooth = 1.5
+model_name = "model3"
+image_name = "ref_image0"
+n = 32
+figname = "figures/br_parameter_visualization_model3.png"
+visualize_observed_and_diffusion(smooth, figname, n, model_name)
