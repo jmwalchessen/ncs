55e722f14bd7468dcba647811f6a1d9676914ace
Julia <jwalches@andrew.cmu.edu>
2024-11-24 16:17:39 +1100

    timing study br unparam

--- brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/timing_study/timing_study.py
+++ brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/timing_study/timing_study.py
@@ -30,27 +30,46 @@ def return_timing(number_of_replicates, smooth_value, range_value, p, n):
     return time_elapsed
 
 
-def plot_sample_size_vs_time(replicates_list, smooth_value, range_value, p, n, figname):
+def plot_sample_size_vs_time(replicates_list, smooth_value, range_value, p, n, figname, npfilename):
 
 
     fig, ax = plt.subplots()
-    timings = []
+    timings = np.zeros((len(replicates_list),2))
     for i in range(0, len(replicates_list)):
 
-        timings[i] = return_timing(replicates_list[i], smooth_value, range_value, p, n)
+        timings[i,0] = replicates_list[i]
+        timings[i,1] = return_timing(replicates_list[i], smooth_value, range_value, p, n)
 
 
-    ax.plot(replicates_list, timings)
+    np.save(npfilename, timings)
+    ax.plot(replicates_list, timings[:,1])
     ax.set_xlabel("Sample Size")
     ax.set_ylabel("Evaluation Time (Seconds)")
     plt.savefig(figname)
 
-def plot_sample_size_vs_time_multiple_percentages(replicates_list, smooth_value, range_value, plist, n, figname):
+def plot_sample_size_vs_time_multiple_percentages(replicates_list, smooth_value, range_value, plist, n, figname, npfilename):
 
 
     for p in plist:
         current_figname = (figname + str(p) + ".png")
-        plot_sample_size_vs_time(replicates_list, smooth_value, range_value, p, n, current_figname)
+        current_npfilename = (npfilename + str(p) + ".npy")
+        plot_sample_size_vs_time(replicates_list, smooth_value, range_value, p, n, current_figname, current_npfilename)
+
+def plot_timing_vs_percentage(smooth_value, range_value, plist, n, figname, npfilename):
+
+    fig, ax = plt.subplots()
+    timings = np.zeros((len(plist),2))
+    for i in range(0, len(plist)):
+
+        timings[i,0] = plist[i]
+        timings[i,1] = return_timing(1, smooth_value, range_value, plist[i], n)
+
+
+    np.save(npfilename, timings)
+    ax.plot(plist, timings[:,1])
+    ax.set_xlabel("Sample Size")
+    ax.set_ylabel("Evaluation Time (Seconds)")
+    plt.savefig(figname)
 
 
 replicates_list = [1,5,10,25,50,100,250,500,1000]
@@ -59,7 +78,9 @@ range_value = 3.0
 plist = [.01,.05,.1,.25,.5]
 n = 32
 figname = "data/model4/timings_smooth_1.5_range_3.0_random"
-plot_sample_size_vs_time(replicates_list, smooth_value, range_value, plist, n, figname)
-
-    
+npfilename = "data/model4/timing_array_smooth_1.5_range_3.0_random"
+plot_sample_size_vs_time(replicates_list, smooth_value, range_value, plist, n, figname, npfilename)
+figname = "data/model4/timings_smooth_1.5_range_3.0_varying_percentage_0105102550.png"
+npfilename = "data/model4/timing_array_smooth_1.5_range_3.0_varying_percentage_0105102550.npy"
+plot_timing_vs_percentage(smooth_value, range_value, plist, n, figname, npfilename)
 
