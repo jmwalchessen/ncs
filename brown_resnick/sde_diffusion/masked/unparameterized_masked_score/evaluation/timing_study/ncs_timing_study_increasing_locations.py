7cc2d7d79d29697e6480b5a9a806365dce7e8cc0
Julia Walchessen <jwalches@andrew.cmu.edu>
2025-03-09 22:38:27 -0400

    ncs timing study

--- brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/timing_study/ncs_timing_study_increasing_locations.py
+++ brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/timing_study/ncs_timing_study_increasing_locations.py
@@ -11,7 +11,7 @@ from helper_functions import *
 import time
 
 #assume we only want to sample once from diffusion model
-def return_timing(smooth_value, range_value, n, number_of_locations):
+def return_timing(smooth_value, range_value, n, number_of_locations, model_name, nrep):
 
     beta_min = .1
     beta_max = 20
@@ -21,33 +21,47 @@ def return_timing(smooth_value, range_value, n, number_of_locations):
     observed_indices = th.from_numpy(np.random.randint(0, n**2, number_of_locations))
     mask[observed_indices] = 1
     mask = mask.reshape((1,1,n,n)).float().to(device)
-    score_model = load_score_model("brown", "model4_beta_min_max_01_20_random01525_smooth_1.5_range_3_channel_mask.pth", "eval")
+    score_model = load_score_model("brown", model_name, "eval")
     vpsde = load_sde(beta_min, beta_max, N)
     seed_value = int(np.random.randint(0, 1000000, 1))
     number_of_replicates = 1
-    y = (th.from_numpy(generate_brown_resnick_process(range_value, smooth_value, seed_value, number_of_replicates, n))).float().to(device)
-    number_of_replicates = 1
-    start = time.time()
-    br_samples = posterior_sample_with_p_mean_variance_via_mask(vpsde, score_model, device, mask,
-                                                            y, n, number_of_replicates)
-    end = time.time()
-    time_elapsed = end - start
+    time_elapsed = np.zeros((nrep))
+    for irep in range(nrep):
+        y = (th.from_numpy(generate_brown_resnick_process(range_value, smooth_value, seed_value, number_of_replicates, n))).float().to(device)
+        number_of_replicates = 1
+        start = time.time()
+        br_samples = posterior_sample_with_p_mean_variance_via_mask(vpsde, score_model, device, mask,
+                                                                    y, n, number_of_replicates)
+        end = time.time()
+        time_elapsed[irep] = end - start
     return time_elapsed
 
-def return_timings(tnrep, smooth_value, range_value, n, number_of_locations_list, timing_file):
+def return_timings(tnrep, smooth_value, range_value, n, number_of_locations_list, timing_file, model_name):
     
     timings = np.zeros((len(number_of_locations_list),(tnrep+1)))
     for i,nlocation in enumerate(number_of_locations_list):
         timings[i,0] = nlocation
-        for t in range(tnrep):
-            timings[i,(t+1)] = return_timing(smooth_value, range_value, n, nlocation)
+        timings[i,1:(tnrep+1)] = return_timing(smooth_value, range_value, n, nlocation, model_name, tnrep)
+
+    np.save(timing_file, timings)
+
+def return_timings_multiple_ranges(tnrep):
+
+    model_names = ["model6/model6_wo_l2_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_1_channel_mask.pth",
+                   "model7/model7_wo_l2_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_2_channel_mask.pth",
+                   "model5/model5_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_3_channel_mask.pth",
+                   "model8/model5_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_4_channel_mask.pth",
+                   "model9/model9_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_5_channel_mask.pth"]
+    model_versions = [6,7,5,8,9]
+    range_values = [float(i) for i in range(1,6)]
+    smooth_value = 1.5
+    n = 31
+    number_of_locations_list = [i for i in range(1,8)]
+    for i,range_value in enumerate(range_values):
+        timing_file = "data/" + model_versions[i] + "_ncs_timing_array_azure_gpu_1_7_nrep_50.npy"
+        return_timings(tnrep, smooth_value, range_value, n, number_of_locations_list,
+                       timing_file, model_names[i])
 
 
-    np.save(timing_file, timing)
-tnrep = 50
-smooth_value = 1.5
-range_value = 3.0
-n = 32
-number_of_locations_list = [1,2,3,4,5,6,7]
-timing_file = "data/model4/ncs_timing_array_ghidorah1_conditional_simulation_1_7_tnrep_50.npy"
-return_timings(tnrep, smooth_value, range_value, n, number_of_locations_list, timing_file)
+
+return_timings_multiple_ranges(50)
