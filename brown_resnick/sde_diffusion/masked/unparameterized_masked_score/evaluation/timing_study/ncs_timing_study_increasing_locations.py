29bdddbb23da476935ac5e093887511a60ccf98b
Julia Walchessen <jwalches@andrew.cmu.edu>
2025-04-19 13:18:18 -0400

    timing study ncs

--- brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/timing_study/ncs_timing_study_increasing_locations.py
+++ brown_resnick/sde_diffusion/masked/unparameterized_masked_score/evaluation/timing_study/ncs_timing_study_increasing_locations.py
@@ -48,15 +48,18 @@ def return_timings(tnrep, smooth_value, range_value, n, number_of_locations_list
 def return_timings_multiple_ranges(tnrep):
 
     model_names = [
+                   "model6/model6_wo_l2_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_1_channel_mask.pth",
+                   "model7/model7_wo_l2_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_2_channel_mask.pth",
+                   "model5/model5_wo_l2_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_3_channel_mask.pth",
                    "model8/model8_wo_l2_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_4_channel_mask.pth",
                    "model9/model9_wo_l2_beta_min_max_01_20_obs_num_1_10_smooth_1.5_range_5_channel_mask.pth"]
-    model_versions = [8,9]
-    range_values = [float(i) for i in range(4,6)]
+    model_versions = [6,7,5,8,9]
+    range_values = [float(i) for i in range(1,6)]
     smooth_value = 1.5
     n = 32
-    number_of_locations_list = [i for i in range(1,8)]
+    number_of_locations_list = [i for i in range(8,11)]
     for i,range_value in enumerate(range_values):
-        timing_file = "data/" + str(model_versions[i]) + "_ncs_timing_array_azure_gpu_1_7_nrep_50.npy"
+        timing_file = "data/" + str(model_versions[i]) + "_ncs_timing_array_azure_gpu_8_10_nrep_50.npy"
         return_timings(tnrep, smooth_value, range_value, n, number_of_locations_list,
                        timing_file, model_names[i])
 
